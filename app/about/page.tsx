64431fd47e0508667055722a9bf47ad5a487115a
Johanso <tipsytutos@gmail.com>
2024-12-08 19:41:59 -0500

    fx errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,12 +14,12 @@ export default function AboutPage() {
       <Title text="About Me" />
 
       <p className="font-[family-name:var(--font-inter)] font-normal mb-4">
-        <strong className="font-bold mb-1 block tracking-wide">Hello! I'm Johan Ricardo.</strong>
+        <strong className="font-bold mb-1 block tracking-wide">Hello! I&apos;m Johan Ricardo.</strong>
         A web designer and developer focused on creating outstanding user experiences. With a background in graphic design and coding skills, I'm here to turn your web visions into reality. Let's chat!
       </p>
       <h3 className="text-md font-semibold tracking-wide text-black mb-4">Is there anything I can do for you?</h3>
       
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mt-6'>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
         <BoxInfoText 
           title="UI Design" 
           icon={<IconBoundingBox size={28} />}
@@ -38,8 +38,8 @@ export default function AboutPage() {
           text="WordPress is my playground when it comes to web development. I excel in customizing, optimizing, and extending WordPress websites."  />
       </div>
 
-      <div className='grid grid-cols-1 gap-4 mt-10'>
-        <h2 className="text-xl lg:text-2xl font-bold mb-4 text-center">Skill's &amp;<span className="text-primary"> Technology</span></h2>
+      <div className="grid grid-cols-1 gap-4 mt-10">
+        <h2 className="text-xl lg:text-2xl font-bold mb-4 text-center">Skill&apos;s &amp;<span className="text-primary"> Technology</span></h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 lg:grid-cols-4">
           <div className="flex items-center px-3 py-3 border rounded-lg bg-white border-light-gray">
             <Image
@@ -48,7 +48,7 @@ export default function AboutPage() {
               height={30}
               alt="HTML 5"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">HTML 5</h3>
           </div>
@@ -59,7 +59,7 @@ export default function AboutPage() {
               height={30}
               alt="CSS 3"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">CSS 3</h3>
           </div>
@@ -70,7 +70,7 @@ export default function AboutPage() {
               height={30}
               alt="Javascript"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">Javascript</h3>
           </div>
@@ -81,7 +81,7 @@ export default function AboutPage() {
               height={30}
               alt="Typescript"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">Javascript</h3>
           </div>
@@ -92,7 +92,7 @@ export default function AboutPage() {
               height={30}
               alt="Sass"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">Sass</h3>
           </div>
@@ -103,7 +103,7 @@ export default function AboutPage() {
               height={30}
               alt="TailwindCss"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">TailwindCss</h3>
           </div>
@@ -114,7 +114,7 @@ export default function AboutPage() {
               height={30}
               alt="Webpack 5"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">Webpack 5</h3>
           </div>
@@ -125,7 +125,7 @@ export default function AboutPage() {
               height={30}
               alt="React JS"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">React JS</h3>
           </div>
@@ -136,7 +136,7 @@ export default function AboutPage() {
               height={30}
               alt="Next JS"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">Next JS</h3>
           </div>
@@ -147,7 +147,7 @@ export default function AboutPage() {
               height={30}
               alt="Figma"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">Figma</h3>
           </div>
@@ -158,7 +158,7 @@ export default function AboutPage() {
               height={30}
               alt="Wordpress"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">Wordpress</h3>
           </div>
@@ -169,7 +169,7 @@ export default function AboutPage() {
               height={30}
               alt="Git"
               loading="lazy"
-              className='w-6 h-6 md:w-8 md:h-8'
+              className="w-6 h-6 md:w-8 md:h-8"
             />
             <h3 className="ml-2 text-xs font-medium text-black md:text-sm md:ml-3">Git</h3>
           </div>
