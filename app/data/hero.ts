3fcd69a4892f5a824d51b918a849059cf0fdcf33
Johanso <tipsytutos@gmail.com>
2025-03-11 22:28:45 -0500

    image profile

--- app/data/hero.ts
+++ app/data/hero.ts
@@ -4,5 +4,5 @@ export const heroData: HeroData = {
   name: "Johan Ricardo N",
   role: "[Frontend UI / DEV]",
   description: "I build sleek and engaging web experiences that make an impact",
-  image: "https://res.cloudinary.com/dcplixyte/image/upload/v1698338685/porfolio/1517282589750_jodhdz.jpg",
+  image: "https://res.cloudinary.com/dcplixyte/image/upload/v1741750030/porfolio/xvv40qc1b3il0ldsdn4y.jpg",
 };
